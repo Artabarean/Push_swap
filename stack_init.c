84617c63137ad3a3a6f9db802245995a70b3aabe
Artabarean <artabarean@gmail.com>
2025-03-19 17:25:09 +0100

    new approach

--- stack_init.c
+++ stack_init.c
@@ -1,35 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   stack_init.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/18 11:30:19 by atabarea          #+#    #+#             */
-/*   Updated: 2025/03/19 17:24:29 by alex             ###   ########.fr       */
+/*   Created: 2025/03/19 17:20:38 by alex              #+#    #+#             */
+/*   Updated: 2025/03/19 17:22:41 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int main(int argc, char *argv[])
+t_list   stack_init(int *sa, char *argv[], int argc)
 {
-	int	*sa;
-	int	*sb;
-	
-	if (argc == 1 || (argc == 2 && !argv[1][0]))
-		return (1);
-	else if (argc == 2)
-		argv = ft_split(argv[1], ' ');
-		stack_init(sa, argv + 1, 2 == argc);
-		if (!stack_sorted(sa))
-		{
-			if (stack_len(sa) == 2)
-				_sa(&sa);
-			else if (stack_len(sa) == 3)
-				tiny_sort(&sa);
-			else
-				push_swap(&sa, &sb);
-		}
-    free_stack(&sa);
+    
 }
