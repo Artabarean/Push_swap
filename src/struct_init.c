d2e628150eaccd32183dbb9cfa1aab2399f1dce9
Alex Robert Tabarean <atabarea@c2r7s6.42madrid.com>
2025-04-02 13:14:51 +0200

    some norm

--- src/struct_init.c
+++ src/struct_init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   struct_init.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: atabarea <atabarea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/25 11:24:38 by alex              #+#    #+#             */
-/*   Updated: 2025/03/28 10:42:09 by alex             ###   ########.fr       */
+/*   Updated: 2025/04/02 13:14:48 by atabarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,8 @@ l_list  *struct_init(int initializer)
     auxvar->median = initializer;
     auxvar->ck_min = initializer;
     auxvar->ck_max = initializer;
-    auxvar->elements_in_ck = initializer;
     auxvar->pushed = initializer;
     auxvar->best_pos = initializer;
-    auxvar->largest_pos = initializer;
-	auxvar->c = initializer;
-    auxvar->i = initializer;
+    auxvar->c = initializer;
     return (auxvar);
 }
